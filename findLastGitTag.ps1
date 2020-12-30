6479d53fff5092049f8b2e7c6b40e7202e098f19
kantarogit <kantarofilip@gmail.com>
2020-12-30 10:17:34 +0100

    ps

--- findLastGitTag.ps1
+++ findLastGitTag.ps1
@@ -1,12 +1,12 @@
-﻿$minors = @()
+$minors = @()
 git fetch origin
-$TAGS = git tag --list "f3*"
+$TAGS = git tag --list "${branchName}"
 foreach ($tag in $TAGS)
 {
     $minors += $tag.SubString($tag.LastIndexOf(".") + 1) -as [int]
 }
 if ($minors.Length -eq 0)
 {
-    $minors.Clear(); $minors += 1 -as [int]
+    $minors.Clear(); $minors += 0 -as [int]
 }
 $minors | sort -descending | select -First 1
